b2abea7d90b25d798cfd02e45183c9634c5b3eee
Artem <mednartem@yandex.ru>
2023-11-12 22:34:11 -0800

    possible fix for the remote run

--- src/test/java/com/portnov/env_sky/logic/driver/DesktopDriver.java
+++ src/test/java/com/portnov/env_sky/logic/driver/DesktopDriver.java
@@ -22,7 +22,7 @@ public class DesktopDriver {
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-notifications");
 
-        String remoteUrl = System.getenv("remoteUrl");
+        String remoteUrl = System.getProperty("remoteUrl");
         if (remoteUrl != null) {
             Configuration.remote = remoteUrl;
             capabilities.setCapability("selenoid:options", Map.<String, Object>of(
