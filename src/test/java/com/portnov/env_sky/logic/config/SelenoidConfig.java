472bd50e8c80f988f0a58b24037336a2d9918136
Artem <mednartem@yandex.ru>
2023-11-17 15:02:22 -0800

    make remote by default

--- src/test/java/com/portnov/env_sky/logic/config/SelenoidConfig.java
+++ src/test/java/com/portnov/env_sky/logic/config/SelenoidConfig.java
@@ -2,10 +2,8 @@ package com.portnov.env_sky.logic.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "classpath:selenoid.properties",
-        "system:properties"
 })
 public interface SelenoidConfig extends Config {
 
