24b774da4efd48c85892eef8c26903632c9ee964
Artem <mednartem@yandex.ru>
2023-11-17 16:23:56 -0800

    removed attachScreenshot method

--- src/test/java/com/portnov/env_sky/logic/attachments/AttachmentHelpers.java
+++ src/test/java/com/portnov/env_sky/logic/attachments/AttachmentHelpers.java
@@ -2,10 +2,7 @@ package com.portnov.env_sky.logic.attachments;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class AttachmentHelpers {
@@ -13,11 +10,6 @@ public class AttachmentHelpers {
     @Attachment(value = "{attachName}", type = "text/plain")
     public static String attachAsText(String attachName, String message) {
         return message;
-    }
-
-    @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[] attachScreenshot(String attachName) {
-        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
     public static String getConsoleLogs() {
